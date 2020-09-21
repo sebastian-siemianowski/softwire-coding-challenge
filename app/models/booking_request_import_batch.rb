6169894945ab23fb7276117759031c45b767443c
sebastiansiemianowski <rudph2@gmail.com>
2020-09-21 12:14:01 +0100

    Created ordering scopes

--- app/models/booking_request_import_batch.rb
+++ app/models/booking_request_import_batch.rb
@@ -10,4 +10,6 @@ class BookingRequestImportBatch < ApplicationRecord
   scope :in_progress, -> { where(status: IN_PROGRESS_STATUS) }
   scope :import_completed, -> { where(status: IMPORT_COMPLETED_STATUS) }
   scope :completed_with_errors, -> { where(status: IMPORT_COMPLETED_WITH_ERRORS_STATUS) }
+  scope :newest_first, -> { order(created_at: :desc) }
+  scope :oldest_first, -> { order(created_at: :asc) }
 end
--- spec/models/booking_request_import_batch_spec.rb
+++ spec/models/booking_request_import_batch_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe BookingRequestImportBatch do
 
   let(:in_progress_batch){ described_class.create!(status: 'in_progress') }
   let(:import_completed_batch){ described_class.create!(status: 'import_completed') }
+  let(:import_completed_batch_2){ described_class.create!(status: 'import_completed') }
   let(:completed_with_errors_batch){ described_class.create!(status: 'completed_with_errors') }
 
   context 'when testing scopes' do
@@ -23,6 +24,14 @@ RSpec.describe BookingRequestImportBatch do
 
     it 'returns completed with errors batches' do
       expect(BookingRequestImportBatch.completed_with_errors.first.status).to eq 'completed_with_errors'
+    end
+
+    it 'allows ordering by newest batches' do
+      expect(BookingRequestImportBatch.newest_first.first.status).to eq 'completed_with_errors'
+    end
+
+    it 'allows ordering by oldest batches' do
+      expect(BookingRequestImportBatch.oldest_first.first.status).to eq 'in_progress'
     end
   end
 end
